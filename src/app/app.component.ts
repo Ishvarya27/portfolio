f833238d89499b5341cc3b6fc3fc855eca9b0a66
Ishvarya27 <ishvaryasathya@gmail.com>
2024-06-30 11:44:07 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  achievements = [
+    {
+      image: '../assets/achievement1.jpg',
+      description: 'Topper in 3rd and 4th semesters of CSE Dept for the academic year 2022-2023! 🏆'
+    },
+    {
+      image: '../assets/achievement2.jpg',
+      description: 'Image Stitching Enhanced Mobile Surveillance System has been officially registered for a UK Design Patent.'
+    },
+    {
+      image: '../assets/achievement3.jpg',
+      description: "Joint Treasurer of the Coders Club at Thiagarajar College of Engineering since October 2023."
+    }
+    // Add more achievements as needed
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
